3926498713ec2f2bb7fb694d93c0e2c891af7583
Senai Mehari <Senai.mehari@stu.ocsb.ca>
2023-10-31 23:34:55 +0000

    dating.py

---
+++ dating.py
@@ -0,0 +1,13 @@
+# Get the user's age as input
+user_age = int(input("Enter you age: "))
+
+# Define the age criteria set by Grandparents #1
+minimum_age = 26
+minimum_age = 39
+maximum_age = 39
+# Check if the user's age falls within the
+# specified range
+if minimum_age <= user_age <= maximum_age:
+    print("You are approved to date their grandchild.")
+else:
+    print("Sorry, you do not meet the age critieria to date their grandchild")
